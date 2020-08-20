fd308d8b859700bd43e75b4873aa419dc5ea3de1
gitfilix <runlevel07@gmail.com>
2020-08-20 21:11:56 +0200

    backend route places with dummy data

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,11 +1,32 @@
 const express = require('express')
+
 const router = express.Router()
 
+// dummy data
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'hundefelsen',
+    description: 'the nicest view from sigriswil',
+    location: {
+      lat:  40.7484474,
+      lng: -74.9871531
+    },
+    address: '21 jump street',
+    creator: 'u1'
+  }
+]
 
 // register middleware on router
-router.get('/', (req, res, next) => {
+// /api/places/p1
+
+router.get('/:pid', (req, res, next) => {
+  const placeId = req.params.pid // { pid: 'p1'}
+  const place = DUMMY_PLACES.find( p => {
+    return p.id === placeId
+  } )
   console.log('GET request on places')
-  res.json({message: 'it should kinda work...'})
+  res.json({ place: place })
 })
 
 
