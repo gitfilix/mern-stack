4d56d7ec1a8f634b6357e473e63760d7b4abeedb
gitfilix <runlevel07@gmail.com>
2020-08-20 17:13:08 +0200

    fast forward to 29-finish auth

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,19 +1,18 @@
-// USER - P A G E - from the pages directory
-import React from 'react'
-import UsersList from '../components/UsersList'
+import React from 'react';
 
+import UsersList from '../components/UsersList';
 
 const Users = () => {
-  // dummy placehoder const
   const USERS = [
     {
       id: 'u1',
-      name: 'filikowski',
+      name: 'FiLiX Admski',
       image: 'https://static.tvtropes.org/pmwiki/pub/images/a3ae1935dee0f88f0681f8246e985fc7.jpg',
       places: 5
     }
-  ]
-  return <UsersList items={USERS} />
-}
+  ];
 
-export default Users
+  return <UsersList items={USERS} />;
+};
+
+export default Users;
