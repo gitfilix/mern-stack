4bf28954aca5824ba98ede72b9fdc462c073297b
gitfilix <runlevel07@gmail.com>
2020-09-26 19:13:20 +0200

    mongoose method get all products

--- backend/mongoose.js
+++ backend/mongoose.js
@@ -11,14 +11,25 @@ mongoose.connect('mongodb+srv://mongodbuser01:77nobigdeal88@cluster0.ajxtu.mongo
 
 
 const createProduct = async (req, res, next) => {
-  const createProduct = new Product({
+  const createdProduct = new Product({
     name: req.body.name,
     price: req.body.price
   })
   // if you have a mongoose model, you can use the method save here! nice!!
   const result = await createdProduct.save()
-
+  // convert the object of id to a string with .id-method
+  // console.log(typeof createdProduct.id)
+  // return result to the calling function
   res.json(result)
 }
 
+// get all products using 'mongoose-find' (returns an array of results)
+const getProducts = async (req, res, next) => {
+  const products = await Product.find().exec()
+  res.json(products)
+}
+
+
+
 exports.createProduct = createProduct
+exports.getProducts = getProducts
