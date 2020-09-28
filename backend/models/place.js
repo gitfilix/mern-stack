bf107f9f1d8857915a1794fb877f44586e350330
gitfilix <runlevel07@gmail.com>
2020-09-28 18:37:29 +0200

    db place mongoose work now

--- backend/models/place.js
+++ backend/models/place.js
@@ -2,18 +2,19 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
+
 const placeSchema = new Schema({
-  title: { type: String, required: true},
-  description: { type: String, required: true},
-  image: { type: String, required: true},
-  adress: { type: String, required: true},
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+  address: { type: String, required: true },
   location: {
     lat: { type: Number, required: true },
     lng: { type: Number, required: true },
   },
-  creator: { type: String, required: true } 
+  creator: { type: String, required: true }
 })
-
 // model-method: returns a constructor function: singular 
-// collection will be a called 'PlaceS'
+// collection Name in the mongo-DB will be a called 'Places' (db-overview)
+
 module.exports = mongoose.model('Place', placeSchema)
