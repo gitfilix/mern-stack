6cf2e2abc8fe33c17dca96a8f7e278e71a0608cf
gitfilix <runlevel07@gmail.com>
2020-12-07 15:38:09 +0100

    finished auto login and auto logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,32 +14,50 @@ import Auth from './user/pages/Auth';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import { AuthContext } from './shared/context/auth-context'
 
+// 'behind the scenes -variable' logoutTimer
+let logoutTimer
 
 const App = () => {
   const [token, setToken] = useState(false)
+  const [tokenExpirationDate, setTokenExpirationDate] = useState()
   const [userId, setUserId] = useState(false)
  
   // run that only on initial load dep-arry: []
   const login = useCallback((uid, token, expirationDate) => {
     setToken(token)
     setUserId(uid)
-    // token expiration Date + now + 1h or valid expiration date from props
-    const tokenExpirationDate = expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60)
-
+    // remainingExpirationTimeString + now + 1h or valid expiration date from props
+    const remainingExpirationTimeString = expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60)
+    
+    setTokenExpirationDate(tokenExpirationDate)
     // localstorage set uid and token
     localStorage.setItem('userData', JSON.stringify({
         userId: uid,
         token: token,
-        expiration: tokenExpirationDate.toISOString()
+        expiration: remainingExpirationTimeString.toISOString()
       })
     )
   }, [])
 
   const logout = useCallback(() => {
     setToken(null)
+    setTokenExpirationDate(null)
     setUserId(null)
     localStorage.removeItem('userData')
   }, [])
+
+  // logout timer hook 
+  useEffect(() => {
+    if (token && tokenExpirationDate ) {
+      // check if tokenExpirationDate is still valid 
+      const remainingTime = tokenExpirationDate.getTime() - new Date().getTime()
+      // again behind the scene variable logoutTimer
+      logoutTimer = setTimeout(logout, remainingTime)
+      console.log('logoutTimer', logoutTimer)
+    } else {
+      clearTimeout(logoutTimer)
+    }
+  }, [token, logout, tokenExpirationDate])
 
   // useEffect runs always After (the first) render-cycle
   // if stored.data for user id -> execute login function
